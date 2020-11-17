3889f7a7ac16f19939ec345562e7b2a809376c01
dmitry <dmitry.suvorov@casamba.net>
2020-11-16 20:33:28 -0800

    Init

---
+++ src/app/shared/widgets/weather/weather.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-widget-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  chartOptions:[];
+
+
+  ngOnInit(): void {
+    this.chartOptions = []
+  }
+
+}
